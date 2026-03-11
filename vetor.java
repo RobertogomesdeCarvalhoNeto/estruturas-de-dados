ca581dd83d23c1019b772e7099e62961fe5a4efe
ROBERTO GOMES DE CARVALHO NETO <roberto.neto005@cs.cruzeirodosul.edu.br>
2026-03-11 10:08:16 -0300

    modificado

--- vetor.java
+++ vetor.java
@@ -9,12 +9,16 @@ public class vetor {
         double a[]={10,20,30,4.3,3.2,-1,-2};
         String msg="Vetor de valores reais";
         visualizaVetor(msg,a);
+        System.out.println();
+        visualizaVetor(msg,a);
     }
 
     public void visualizaVetor(String mensagem, double vet[]){
         System.out.println(mensagem);
+        mensagem="vetorizado";
         for (int i=0; i<vet.length; i++){
             System.err.print(vet[i]+ "||");
+            vet[i]=vet[i]*2;
         }
     }
 }
