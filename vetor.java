1ad98343b5cc4aa98622fd46a6d9f7784be9857a
ROBERTO GOMES DE CARVALHO NETO <roberto.neto005@cs.cruzeirodosul.edu.br>
2026-03-11 10:50:21 -0300

    segunda mod

--- vetor.java
+++ vetor.java
@@ -7,18 +7,26 @@ public class vetor {
     }
     public vetor(){
         double a[]={10,20,30,4.3,3.2,-1,-2};
+        
         String msg="Vetor de valores reais";
-        visualizaVetor(msg,a);
+        //visualizaVetor(msg,a);
         System.out.println();
-        visualizaVetor(msg,a);
+        calculaSomaDosElementos(a);
     }
 
+    public void calculaSomaDosElementos(double vet[]){
+        double soma=0;
+             for (int i=0; i<vet.length; i++){
+                 soma=soma+vet[i];
+             }
+             System.err.println("soma dos valores"+soma);
+    }
     public void visualizaVetor(String mensagem, double vet[]){
         System.out.println(mensagem);
-        mensagem="vetorizado";
+        //mensagem="vetorizado";
         for (int i=0; i<vet.length; i++){
             System.err.print(vet[i]+ "||");
-            vet[i]=vet[i]*2;
+            //vet[i]=vet[i]*2;
         }
     }
 }
