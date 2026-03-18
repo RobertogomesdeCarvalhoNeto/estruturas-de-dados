18f5d9b52984d8a97e9af7c0cd1aee68157dc12d
ROBERTO GOMES DE CARVALHO NETO <roberto.neto005@cs.cruzeirodosul.edu.br>
2026-03-18 09:06:39 -0300

    primeiro commit

---
+++ cidade.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class cidade {
+    public static void main(String[] args) {
+        ArrayList<String>cidade=new ArrayList<String>();
+        cidade.add("São Paulo");
+        cidade.add("Rio de Janeiro");
+        cidade.add("Brasília");
+        cidade.add("Belo Horizonte");
+        cidade.add("Santos");
+        System.out.println(cidade);
+    }
+}
