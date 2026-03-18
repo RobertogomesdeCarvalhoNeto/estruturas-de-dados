82d5939030d84dc01e3a9bdcb1b8eb4526ebce5c
ROBERTO GOMES DE CARVALHO NETO <roberto.neto005@cs.cruzeirodosul.edu.br>
2026-03-18 09:51:28 -0300

    busca sequencial

---
+++ buscas.java
@@ -0,0 +1,21 @@
+public class buscas {
+    public static void main(String[] args) {
+        
+        //Busca sequencial
+        double[] vetorDesordenado={4.3, 6.1, 0.1, 1.9, 2.4, 7.2, 5.5};
+
+
+        int pos = buscaSequencial(vetorDesordenado, 2.4);
+        System.out.println("localizado na posição (indice): "+pos);
+    }
+    
+
+    public static int buscaSequencial(double[] vetor, double valorPesquisado){
+        for(int i=0; i<vetor.length; i++){
+           if (vetor[i]==valorPesquisado){
+            return i;
+           } 
+        }
+        return -1;
+    }
+}
