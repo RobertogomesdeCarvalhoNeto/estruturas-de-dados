72b6c9f5fae1e5ce6b6ed0d98bc57dcec10156b2
ROBERTO GOMES DE CARVALHO NETO <roberto.neto005@cs.cruzeirodosul.edu.br>
2026-03-18 10:23:11 -0300

    busca binária e sequencial

--- buscas.java
+++ buscas.java
@@ -3,10 +3,13 @@ public class buscas {
         
         //Busca sequencial
         double[] vetorDesordenado={4.3, 6.1, 0.1, 1.9, 2.4, 7.2, 5.5};
-
-
         int pos = buscaSequencial(vetorDesordenado, 2.4);
         System.out.println("localizado na posição (indice): "+pos);
+
+        //busca binária
+        double[] vetorOrdenado={1, 2, 4, 8, 10, 11, 12};
+        pos=buscaBinaria(vetorOrdenado,1);
+        System.out.println("localizado no vetor ordenado na posição: "+pos);
     }
     
 
@@ -15,6 +18,25 @@ public class buscas {
            if (vetor[i]==valorPesquisado){
             return i;
            } 
+        }
+        return -1;
+    }
+
+    public static int buscaBinaria(double[]vetor, double valorPesquisado){
+        int inicio = 0;
+        int fim = vetor.length -1;
+
+        while(inicio <= fim){
+            int meio = (inicio+fim)/2;
+
+            if (vetor[meio] == valorPesquisado){
+                return meio;
+
+            }else if (valorPesquisado < vetor[meio]){
+                fim = meio -1;
+            }else{
+                inicio = meio +1;
+            }
         }
         return -1;
     }
