d73ffd5f7c92e3018fa449e08922eac138db2ccd
ROBERTO GOMES DE CARVALHO NETO <roberto.neto005@cs.cruzeirodosul.edu.br>
2026-03-18 09:50:28 -0300

    lista vertical

--- cidade.java
+++ cidade.java
@@ -2,12 +2,31 @@ import java.util.ArrayList;
 
 public class cidade {
     public static void main(String[] args) {
-        ArrayList<String>cidade=new ArrayList<String>();
-        cidade.add("São Paulo");
-        cidade.add("Rio de Janeiro");
-        cidade.add("Brasília");
-        cidade.add("Belo Horizonte");
-        cidade.add("Santos");
-        System.out.println(cidade);
+        ArrayList<String>cidades=new ArrayList<String>();
+        cidades.add("São Paulo");
+        cidades.add("Rio de Janeiro");
+        cidades.add("Brasília");
+        cidades.add("Belo Horizonte");
+        cidades.add("Santos");
+        System.out.println(cidades);
+
+        //remove um elemento
+        cidades.remove(1);
+        System.out.println(cidades);
+
+        //exibe a quantidade de elementos
+        System.out.println("numero de cidades: "+cidades.size());
+
+        //exibe em forma de lista (vertical)
+        System.out.println("Lista de cidades com laço FOR");
+        for (int i=0; i<cidades.size(); i++){
+            System.out.println(cidades.get(i)); 
+        }
+
+        //modo mais facil da lista vertical
+        System.out.println("Lista de cidades com laço FOR-EACH");
+        for (String cidade:cidades){
+            System.out.println(cidade);
+        }
     }
 }
