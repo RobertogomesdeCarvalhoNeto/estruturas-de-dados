b71b5e20ed8ad7b1cac4d12dc910603d49629b07
ROBERTO GOMES DE CARVALHO NETO <roberto.neto005@cs.cruzeirodosul.edu.br>
2026-04-08 09:54:22 -0300

    maximo

--- Maximo.java
+++ Maximo.java
@@ -5,7 +5,20 @@ public class Maximo {
     public Maximo(){
         int a[] = {12, 34, 56, 787, 90, 54, 7 ,125, 747, 876, 9765, 92545};
         System.out. println("Maior dos valores 1: " + maximoIterativo(a));
+        System.out.println("Maior dosa valores 2: " + maximoRecursivo(a,0,a.length-1,0));
+        System.out.println("Maior dos valoires 3: "+maximoRecursivo(a,0,3,0));
     }
+
+    public int maximoRecursivo(int vet[], int inicio, int fim, int nivel){
+        if(inicio==fim)return vet[inicio];
+        int meio= (inicio+fim)/2;
+        int a = maximoRecursivo(vet, inicio, meio,++nivel);
+        int b= maximoRecursivo(vet,meio+1,fim,nivel);
+        System.out.println("nivel "+nivel+ ": "+a+">"+b);
+        return ((a>b)?a:b);
+    }
+    
+    
     public int maximoIterativo (int vet[]){
         int max = vet[0];
         for (int i = 1; i<vet.length;i++){
